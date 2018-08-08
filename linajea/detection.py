0ed967b7e856afb53b4449e15caf4c6baf869038
Jan Funke <jfunke@iri.upc.edu>
2018-08-08 15:02:32 -0400

    Rewrite find_edges using peach.Array

--- linajea/detection.py
+++ linajea/detection.py
@@ -1,8 +1,11 @@
 from .nms import find_maxima, sphere
+from .target_counts import target_counts
+from scipy.ndimage.filters import gaussian_filter
 from skimage.measure import block_reduce
 import logging
 import math
 import numpy as np
+import peach
 import time
 
 logger = logging.getLogger(__name__)
@@ -36,78 +39,128 @@ def find_cells(
 
 def find_edges(
         parent_vectors,
-        voxel_size,
-        cell_centers,
+        cells,
         move_threshold,
         radius,
         sigma):
+    '''Find and score edges between cells.
 
+    Args:
+
+        parent_vectors (``peach.Array``):
+
+            An array of predicted parent vectors.
+
+        cells (``dict``):
+
+            Dict from ``id: center`` of each cell.
+
+        move_threshold (``float``):
+
+            By how much cells are allowed spatially between frames.
+
+        radius (``tuple`` of ``int``):
+
+            The 3D radius of a cell to consider to pool parent vectors from. The
+            parent vectors within radius will be used to compute the target
+            counts between two cells, i.e, how many parent vectors from one cell
+            point to the center of the other cell.
+
+        sigma (``float``):
+
+            By how much to smooth the target counts.
+    '''
+
+    assert parent_vectors.roi.dims() == 4, (
+        "Expect 4D input.")
+
+    t_begin = parent_vectors.roi.get_begin()[0]
+    t_end = parent_vectors.roi.get_end()[0]
+
+    # sort cells by frame
     cells_by_t = {
         t: [
             label
-            for label, d in cell_centers.items()
-            if d['center'][0] == t
+            for label, center in cells.items()
+            if center[0] == t
         ]
-        for t in range(parent_vectors.shape[0])
+        for t in range(t_begin, t_end)
     }
-
-    radius_vx = tuple(
-        int(math.ceil(r*v))
-        for r, v in zip(radius, voxel_size))
-    mask = sphere(radius_vx)
 
     logger.debug("Got cells in frames %s", cells_by_t.keys())
 
+    voxel_size_3d = parent_vectors.voxel_size[1:]
+
+    # create a 3D mask centered at (0, 0, 0) to pool parent vectors from
+    radius_vx = peach.Coordinate(
+        int(math.ceil(r/v))
+        for r, v in zip(radius, voxel_size_3d))
+    shape = radius_vx*2 + (1, 1, 1)
+    mask_roi = peach.Roi(
+        (0, 0, 0),
+        shape*voxel_size_3d)
+    mask_roi -= mask_roi.get_center()
+    mask = peach.Array(
+        sphere(radius_vx).astype(np.int32),
+        mask_roi,
+        voxel_size_3d)
+
     edges = []
-    logger.debug("Shape of parent_vectors: %s", parent_vectors.shape)
-    for t in range(0, parent_vectors.shape[1] - 1):
+    for t in range(t_begin, t_end - 1):
 
         pre = t
         nex = t + 1
 
         logger.debug(
-            "Finding edges between cells in frames %d and %d",
-            pre, nex)
+            "Finding edges between cells in frames %d and %d (%d and %d cells)",
+            pre, nex, len(cells_by_t[pre]), len(cells_by_t[nex]))
 
         for pre_cell in cells_by_t[pre]:
             for nex_cell in cells_by_t[nex]:
 
-                pre_center = np.array(cell_centers[pre_cell]['center'])
-                nex_center = np.array(cell_centers[pre_cell]['center'])
+                pre_center = np.array(cells[pre_cell][1:])
+                nex_center = np.array(cells[nex_cell][1:])
 
-                moved = (pre_center - nex_center)*voxel_size
+                moved = (pre_center - nex_center)*voxel_size_3d
                 distance = np.linalg.norm(moved)
 
-                logger.debug(
-                    "Considering edge between %s at %s and %s at %s "
-                    "with distance %f",
-                    pre_cell, pre_center, nex_cell, nex_center, distance)
-
                 if distance > move_threshold:
-                    logger.debug("distance exceeds threshold")
                     continue
 
-                # get score from nex to pre (backwards in time)
+                # Get score from nex to pre (backwards in time).
+                #
+                # Cut out parent vectors around mask_roi centered at nex_center.
+                # We set the fill_value to 1000 to make sure that out-of-bounds
+                # voxels don't contribute to the target counts (they will point
+                # very far away).
 
-                # cut out parent vectors around nex
-                nex_center = tuple(int(c) for c in nex_center)
-                slices = tuple(
-                    slice(c - r, c + r)
-                    for c, r in zip(nex_center, radius_vx)
-                )
-                nex_parent_vectors = parent_vectors[(slice(t + 1),) + slices]
-
-                assert nex_parent_vectors.shape == mask.shape
+                # get the mask ROI around the next cell in 3D
+                nex_roi_3d = mask_roi + peach.Coordinate(nex_center)
+                # add the time dimension
+                nex_roi_4d = peach.Roi(
+                    (nex,) + nex_roi_3d.get_begin(),
+                    (1,) + nex_roi_3d.get_shape())
+                nex_parent_vectors = parent_vectors.fill(
+                    nex_roi_4d,
+                    fill_value=1000)
 
                 # get smoothed target counts at pre
-                counts = target_counts(nex_parent_vectors, mask)
+                assert nex_parent_vectors.shape[1] == 1
+                counts = target_counts(
+                    nex_parent_vectors.data[:,0,:].astype(np.int32),
+                    mask.data)
+                assert len(counts.shape) == 3
                 counts = gaussian_filter(counts, sigma, mode='constant')
-                score = counts[radius_vx[0], radius_vx[1], radius_vx[2]]
+                counts = peach.Array(
+                    counts,
+                    nex_roi_3d,
+                    voxel_size_3d)
+                score = counts[counts.roi.get_center()]
 
                 edges.append({
-                    'from': nex_cell,
-                    'to': pre_cell,
-                    'score': score
+                    'source': nex_cell,
+                    'target': pre_cell,
+                    'score': float(score)
                 })
 
     return edges
