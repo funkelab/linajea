0b9fabbb256af292474cd56a2ef7e770381e8820
Jan Funke <jfunke@iri.upc.edu>
2018-08-10 10:03:27 -0400

    Pass peach.Array to find_maxima, return absolute coordinates

--- linajea/nms.py
+++ linajea/nms.py
@@ -2,47 +2,81 @@ from scipy.ndimage import measurements, label, maximum_filter
 from scipy.ndimage.filters import gaussian_filter
 import math
 import numpy as np
+import peach
 import time
 
 def find_maxima(
-        predictions,
-        voxel_size,
+        array,
         radius,
         sigma=None,
         min_score_threshold=0):
     '''Find all points that are maximal within a sphere of ``radius`` and are
     strictly higher than min_score_threshold. Optionally smooth the prediction
-    with sigma.'''
+    with sigma.
 
-    # smooth predictions
+    Args:
+
+        array (`class:peach.Array`):
+
+            The array to find maxima in.
+
+        radius (``tuple`` of ``int``):
+
+            The radius (in world units) of a sphere to find maxima within.
+
+        sigma (``tuple`` of ``float``, optional):
+
+            By how much to smooth the values in ``array`` before finding maxima.
+
+        min_score_threshold (``float``, optional):
+
+            Only consider maxima with a value grater than this threshold.
+
+    Returns:
+
+        ``(centers, labels, array)``
+
+        ``centers`` a dictionary from maxima IDs to a dictionary with 'center'
+        and 'score', ``labels`` an array of the maximal points with their IDs,
+        and ``array`` the (possibly smoothed) input array.
+        '''
+
+    values = array.data
+
+    # smooth values
     if sigma is not None:
-        print("Smoothing predictions...")
-        sigma = tuple(float(s)/r for s, r in zip(sigma, voxel_size))
+        print("Smoothing values...")
+        sigma = tuple(
+            float(s)/r
+            for s, r in zip(sigma, array.voxel_size))
         print("voxel-sigma: %s"%(sigma,))
         start = time.time()
-        predictions = gaussian_filter(predictions, sigma, mode='constant')
+        values = gaussian_filter(
+            values,
+            sigma,
+            mode='constant')
         print("%.3fs"%(time.time()-start))
 
     print("Finding maxima...")
     start = time.time()
     radius = tuple(
         int(math.ceil(float(ra)/re))
-        for ra, re in zip(radius, voxel_size))
+        for ra, re in zip(radius, array.voxel_size))
     print("voxel-radius: %s"%(radius,))
-    max_filtered = maximum_filter(predictions, footprint=sphere(radius))
+    max_filtered = maximum_filter(values, footprint=sphere(radius))
 
-    maxima = max_filtered == predictions
+    maxima = max_filtered == values
     print("%.3fs"%(time.time()-start))
 
     print("Applying NMS...")
     start = time.time()
-    predictions_filtered = np.zeros_like(predictions)
-    predictions_filtered[maxima] = predictions[maxima]
+    values_filtered = np.zeros_like(values)
+    values_filtered[maxima] = values[maxima]
     print("%.3fs"%(time.time()-start))
 
     print("Finding blobs...")
     start = time.time()
-    blobs = predictions_filtered > min_score_threshold
+    blobs = values_filtered > min_score_threshold
     labels, num_blobs = label(blobs, output=np.uint64)
     print("%.3fs"%(time.time()-start))
 
@@ -52,16 +86,24 @@ def find_maxima(
     start = time.time()
     label_ids = np.arange(1, num_blobs + 1)
     centers = measurements.center_of_mass(blobs, labels, index=label_ids)
-    sizes = measurements.sum(blobs, labels, index=label_ids)
-    maxima = measurements.maximum(predictions, labels, index=label_ids)
+    maxima = measurements.maximum(values, labels, index=label_ids)
     print("%.3fs"%(time.time()-start))
 
     centers = {
-        label: { 'center': center, 'score': max_value }
-        for label, center, size, max_value in zip(label_ids, centers, sizes, maxima)
+        label: {
+            'center': np.array([
+                c*v + o
+                for c, v, o in zip(center, array.voxel_size, array.roi.get_offset())
+            ]),
+            'score': max_value
+        }
+        for label, center, max_value in zip(label_ids, centers, maxima)
     }
 
-    return (centers, labels, predictions)
+    labels = peach.Array(labels, array.roi, array.voxel_size)
+    values = peach.Array(values, array.roi, array.voxel_size)
+
+    return (centers, labels, values)
 
 def sphere(radius):
 
