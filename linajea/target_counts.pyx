2a613516ac930cbf8f323ad3257c73a873a2fbc4
Jan Funke <jfunke@iri.upc.edu>
2018-08-11 13:51:02 -0400

    Correct for voxel size in target_counts from parent vectors

--- linajea/target_counts.pyx
+++ linajea/target_counts.pyx
@@ -1,9 +1,63 @@
 cimport cython
+import logging
 import numpy as np
+
+logger = logging.getLogger(__name__)
+
+def target_counts(parent_vectors, voxel_size, mask=None):
+    '''Given an array of parent vectors, counts for each voxel how many parent
+    vectors point to it. Parent vectors are relative, i.e., an entry
+    ``parent_vectors[:,z,y,x] == [0, 0, 0]`` points to ``[z,y,x]``,
+    ``[1, 0, 0]`` points to ``[z+1,y,x]``.
+
+    Args:
+
+        parent_vectors (``ndarray``):
+
+            Array of shape ``(3, d, h, w)`` containing parent vectors.
+
+        voxel_size (``tuple`` of ``int``):
+
+            The size of a voxel in world units.
+
+        mask (``ndarray``, optional):
+
+            A mask to count only offset vectors for voxels with value 1 in the
+            mask.
+
+    Returns:
+
+        An ``ndarray`` of shape (d, h, w), containing the number of offset
+        vectors pointing to the respective location.
+    '''
+
+    # make sure the inputs are all 3D
+    assert len(parent_vectors.shape) == 4, (
+        "parent_vectors needs to be a 4d array")
+    assert parent_vectors.shape[0] == 3, (
+        "parent vectors should be 3D vectors")
+    assert len(voxel_size) == 3, "voxel size is not 3D"
+
+    logger.info(
+        "counting targets in parent vectors of shape %s with voxel size %s",
+        parent_vectors.shape, voxel_size)
+
+    # (3, d, h, w)
+    offset_vectors = np.array(parent_vectors)
+
+    # convert offsets from world units to voxels
+    offset_vectors[0] /= voxel_size[0]
+    offset_vectors[1] /= voxel_size[1]
+    offset_vectors[2] /= voxel_size[2]
+
+    # discretize offset vectors
+    offset_vectors = np.round(offset_vectors).astype(np.int32)
+
+    return target_counts_from_offsets(offset_vectors, mask)
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def target_counts(offsets, mask=None):
+def target_counts_from_offsets(offsets, mask=None):
     '''Given a volume of offsets, counts for each voxel how many offsets point
     to it. Offsets are relative, i.e., an entry ``offsets[:,z,y,x] == [0, 0,
     0]`` points to ``[z,y,x]``, ``[1, 0, 0]`` points to ``[z+1,y,x]``.
@@ -13,6 +67,11 @@ def target_counts(offsets, mask=None):
         offsets (``ndarray``):
 
             Array of shape ``(3, d, h, w)`` containing offset vectors.
+
+        mask (``ndarray``, optional):
+
+            A mask to count only offset vectors for voxels with value 1 in the
+            mask.
 
     Returns:
 
