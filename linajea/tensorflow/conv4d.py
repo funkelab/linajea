26a37ac8fd3bd11c536c8f7c2584d6b407a39695
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2018-11-28 14:38:14 -0500

    Python3 integer division in conv4d

--- linajea/tensorflow/conv4d.py
+++ linajea/tensorflow/conv4d.py
@@ -134,7 +134,7 @@ def conv4d(
         for j in range(l_i):
 
             # add results to this output frame
-            out_frame = j - (i - l_k/2) - (l_i - l_o)/2
+            out_frame = j - (i - l_k//2) - (l_i - l_o)//2
             if out_frame < 0 or out_frame >= l_o:
                 continue
 
