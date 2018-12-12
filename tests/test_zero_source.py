d0308271bcb9abe48d670f1a962c89b92031f953
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2018-12-12 11:36:52 -0500

    Add zero source to avoid predicting empty blocks

---
+++ tests/test_zero_source.py
@@ -0,0 +1,25 @@
+import gunpowder as gp
+import numpy as np
+from linajea.gunpowder import ZeroSource
+
+if __name__=="__main__":
+    #test zero source
+    parent_vectors = gp.ArrayKey('PARENT_VECTORS')
+    roi = gp.Roi(offset=gp.Coordinate((0,0,0,0)), shape=gp.Coordinate((10, 10, 10, 10)))
+    pipeline = (
+            ZeroSource(
+                parent_vectors,
+                gp.ArraySpec(voxel_size=gp.Coordinate((1,5,1,1)),
+                    dtype=np.float32)
+                ) # + 
+            # gp.Crop(parent_vectors, roi)
+            )
+    with gp.build(pipeline):
+        request = gp.BatchRequest()
+        request.add(parent_vectors, roi.get_shape())
+        b = pipeline.request_batch(request)
+        data = b[parent_vectors].data
+        print(data.shape)
+        print(np.any(data))
+        print(data)
+
