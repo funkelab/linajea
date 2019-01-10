6d5fd8e0ec2c5c2b38e8ee9f18ddb8b5724986ab
Jan Funke <funkej@janelia.hhmi.org>
2019-01-09 19:18:38 -0500

    Add gunpowder node WriteCells

---
+++ linajea/gunpowder/write_cells.py
@@ -0,0 +1,73 @@
+from funlib import math
+import gunpowder as gp
+import numpy as np
+import pymongo
+
+
+class WriteCells(gp.BatchFilter):
+
+    def __init__(
+            self,
+            maxima,
+            cell_indicator,
+            parent_vectors,
+            score_threshold,
+            db_host,
+            db_name,
+            db_user=None,
+            db_password=None):
+
+        self.maxima = maxima
+        self.cell_indicator = cell_indicator
+        self.parent_vectors = parent_vectors
+        self.score_threshold = score_threshold
+        self.db_host = db_host
+        self.db_name = db_name
+        self.db_user = db_user
+        self.db_password = db_password
+        self.client = None
+
+    def process(self, batch, request):
+
+        if self.client is None:
+            self.client = pymongo.MongoClient(
+                host=self.db_host,
+                username=self.db_user,
+                password=self.db_password)
+            self.db = self.client[self.db_name]
+            self.cells = self.db['nodes']
+
+        roi = batch[self.maxima].spec.roi
+        voxel_size = batch[self.maxima].spec.voxel_size
+
+        maxima = batch[self.maxima].data
+        cell_indicator = batch[self.cell_indicator].data
+        parent_vectors = batch[self.parent_vectors].data
+
+        cells = []
+        for index in np.argwhere(maxima*cell_indicator > self.score_threshold):
+
+            index = gp.Coordinate(index)
+
+            score = cell_indicator[index]
+            parent_vector = parent_vectors[(Ellipsis,) + index]
+            position = roi.get_begin() + voxel_size*index
+
+            cell_id = math.cantor_number(roi.get_begin()/voxel_size + index)
+
+            cells.append({
+                'id': cell_id,
+                'score': score,
+                't': position[0],
+                'z': position[1],
+                'y': position[2],
+                'x': position[3],
+                'parent_vector': tuple(parent_vector)
+            })
+
+            print(
+                "ID=%d, score=%f, parent_vector=%s" % (
+                    cell_id, score, parent_vector))
+
+        if len(cells) > 0:
+            self.cells.insert_many(cells)
