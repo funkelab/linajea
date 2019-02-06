2b81eb23da6aa56f4bb4792798ce3673e21fa713
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2019-02-06 17:48:35 -0500

    Shift only between frames -1 and 0

--- linajea/gunpowder/__init__.py
+++ linajea/gunpowder/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 from .add_parent_vectors import AddParentVectors
-from .jitter import Jitter
+from .shift_augment import ShiftAugment
 from .random_location_exclude_time import RandomLocationExcludeTime
 from .tracks_source import TracksSource
 from .write_cells import WriteCells
