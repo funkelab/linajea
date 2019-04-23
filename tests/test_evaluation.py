ad38bbc5428390088d04a7c258156c5dca311e37
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2019-04-23 01:04:41 -0400

    Update evaluate tests

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -67,251 +67,95 @@ class EvaluationTestCase(unittest.TestCase):
         roi = daisy.Roi((0, 0, 0, 0), (5, 5, 5, 5))
         return cells, edges, roi
 
-    def test_perfect_matching(self):
-        cells, edges, roi = self.getTrack1()
-        gt_tracks = self.get_tracks(cells, edges, roi)
-        for cell in cells:
-            cell[1]['y'] += 1
-        rec_tracks = self.get_tracks(cells, edges, roi)
-        match_output = e.match_tracks(gt_tracks, rec_tracks, 2)
-        track_matches, cell_matches, splits, merges, fp, fn = match_output
-
-        expected_track_matches = [(0, 0)]
-        expected_cell_matches = [
-                (1, 1),
-                (2, 2),
-                (3, 3),
-                (4, 4)
-                ]
-
-        self.assertCountEqual(track_matches, expected_track_matches)
-        self.assertCountEqual(cell_matches, expected_cell_matches)
-        self.assertEqual(splits, 0)
-        self.assertEqual(merges, 0)
-        self.assertEqual(fp, 0)
-        self.assertEqual(fn, 0)
-
     def test_perfect_evaluation(self):
         cells, edges, roi = self.getTrack1()
-        gt_tracks = self.get_tracks(cells, edges, roi)
+        gt_track_graph = self.create_graph(cells, edges, roi)
         for cell in cells:
             cell[1]['y'] += 1
-        rec_tracks = self.get_tracks(cells, edges, roi)
+        rec_track_graph = self.create_graph(cells, edges, roi)
+        scores = e.evaluate(
+                gt_track_graph, rec_track_graph, matching_threshold=2)
 
-        scores = e.evaluate(gt_tracks, rec_tracks, 2)
-        self.assertEqual(scores.num_gt_tracks, 1)
-        self.assertEqual(scores.num_matched_tracks, 1)
-        self.assertEqual(scores.num_splits, 0)
-        self.assertEqual(scores.num_merges, 0)
-        self.assertEqual(scores.num_fp_tracks, 0)
-        self.assertEqual(scores.num_fn_tracks, 0)
-
-        self.assertEqual(scores.num_gt_nodes, 4)
-        self.assertEqual(scores.num_matched_nodes, 4)
-        self.assertEqual(scores.num_fp_nodes, 0)
-        self.assertEqual(scores.num_fn_nodes, 0)
-
-        self.assertEqual(scores.num_gt_edges, 3)
         self.assertEqual(scores.num_matched_edges, 3)
         self.assertEqual(scores.num_fp_edges, 0)
         self.assertEqual(scores.num_fn_edges, 0)
-
-        self.assertEqual(scores.num_gt_divisions, 0)
-        self.assertEqual(scores.num_matched_divisions, 0)
-        self.assertEqual(scores.num_fp_divisions, 0)
-        self.assertEqual(scores.num_fn_divisions, 0)
-
-    def test_imperfect_matching(self):
-        cells, edges, roi = self.getTrack1()
-        gt_tracks = self.get_tracks(cells, edges, roi)
-        for cell in cells:
-            cell[1]['y'] += 1
-        # introduce a split error
-        del edges[1]
-        rec_tracks = self.get_tracks(cells, edges, roi)
-        match_output = e.match_tracks(gt_tracks, rec_tracks, 2)
-        track_matches, cell_matches, splits, merges, fp, fn = match_output
-
-        expected_track_matches = [(0, 0), (0, 1)]
-        expected_cell_matches = [
-                (1, 1),
-                (2, 2),
-                (3, 3),
-                (4, 4)
-                ]
-
-        self.assertCountEqual(track_matches, expected_track_matches)
-        self.assertCountEqual(cell_matches, expected_cell_matches)
-        self.assertEqual(splits, 1)
-        self.assertEqual(merges, 0)
-        self.assertEqual(fp, 0)
-        self.assertEqual(fn, 0)
+        self.assertEqual(scores.num_gt_tracks, 1)
+        self.assertEqual(scores.num_gt_matched_tracks, 1)
+        self.assertEqual(scores.num_rec_matched_tracks, 1)
+        self.assertEqual(scores.num_rec_tracks, 1)
+        self.assertEqual(scores.num_edge_fps_in_matched_tracks, 0)
+        self.assertEqual(scores.avg_segment_length, 3)
+        self.assertEqual(scores.num_identity_switches, 0)
 
     def test_imperfect_evaluation(self):
         cells, edges, roi = self.getTrack1()
-        gt_tracks = self.get_tracks(cells, edges, roi)
+        gt_track_graph = self.create_graph(cells, edges, roi)
         for cell in cells:
             cell[1]['y'] += 1
         # introduce a split error
         del edges[1]
-        rec_tracks = self.get_tracks(cells, edges, roi)
+        rec_track_graph = self.create_graph(cells, edges, roi)
+        scores = e.evaluate(
+                gt_track_graph, rec_track_graph, matching_threshold=2)
 
-        scores = e.evaluate(gt_tracks, rec_tracks, 2)
-        self.assertEqual(scores.num_gt_tracks, 1)
-        self.assertEqual(scores.num_matched_tracks, 2)
-        self.assertEqual(scores.num_splits, 1)
-        self.assertEqual(scores.num_merges, 0)
-        self.assertEqual(scores.num_fp_tracks, 0)
-        self.assertEqual(scores.num_fn_tracks, 0)
-
-        self.assertEqual(scores.num_gt_nodes, 4)
-        self.assertEqual(scores.num_matched_nodes, 4)
-        self.assertEqual(scores.num_fp_nodes, 0)
-        self.assertEqual(scores.num_fn_nodes, 0)
-
-        self.assertEqual(scores.num_gt_edges, 3)
         self.assertEqual(scores.num_matched_edges, 2)
         self.assertEqual(scores.num_fp_edges, 0)
         self.assertEqual(scores.num_fn_edges, 1)
-
-        self.assertEqual(scores.num_gt_divisions, 0)
-        self.assertEqual(scores.num_matched_divisions, 0)
-        self.assertEqual(scores.num_fp_divisions, 0)
-        self.assertEqual(scores.num_fn_divisions, 0)
-
-    def test_division_matching(self):
-        cells, edges, roi = self.getDivisionTrack()
-        # introduce a false positive edge
-        gt_cells = cells.copy()
-        gt_edges = edges.copy()
-        del gt_cells[0]
-        del gt_edges[0]
-        gt_tracks = self.get_tracks(gt_cells, gt_edges, roi)
-        for cell in cells:
-            cell[1]['y'] += 1
-        # introduce a split error
-        edges.remove((6, 5))
-        rec_tracks = self.get_tracks(cells, edges, roi)
-        match_output = e.match_tracks(gt_tracks, rec_tracks, 2)
-        track_matches, cell_matches, splits, merges, fp, fn = match_output
-
-        expected_track_matches = [(0, 0), (0, 1)]
-        expected_cell_matches = [
-                (2, 2),
-                (3, 3),
-                (4, 4),
-                (5, 5),
-                (6, 6),
-                (7, 7)
-                ]
-
-        self.assertCountEqual(track_matches, expected_track_matches)
-        self.assertCountEqual(cell_matches, expected_cell_matches)
-        self.assertEqual(splits, 1)
-        # self.assertEqual(merges, 0) with quadmatch and my intuition
-        self.assertEqual(merges, 1)  # with comatch and Jan's intuition
-        # self.assertEqual(fp, 0) with quadmatch and my intuition
-        self.assertEqual(fp, 1)  # with comatch
-        self.assertEqual(fn, 0)
-
-    def test_division_evaluation(self):
-        cells, edges, roi = self.getDivisionTrack()
-        # introduce a false positive edge
-        gt_cells = cells.copy()
-        gt_edges = edges.copy()
-        del gt_cells[0]
-        del gt_edges[0]
-        gt_tracks = self.get_tracks(gt_cells, gt_edges, roi)
-        for cell in cells:
-            cell[1]['y'] += 1
-        # introduce a split error
-        edges.remove((6, 5))
-        rec_tracks = self.get_tracks(cells, edges, roi)
-
-        scores = e.evaluate(gt_tracks, rec_tracks, 2)
         self.assertEqual(scores.num_gt_tracks, 1)
-        self.assertEqual(scores.num_matched_tracks, 2)
-        self.assertEqual(scores.num_splits, 1)
-        # self.assertEqual(scores.num_merges, 0) with quadmatch/my intuition
-        self.assertEqual(scores.num_merges, 1)  # with comatch
-        # self.assertEqual(scores.num_fp_tracks, 0) with quadmatch/my intuition
-        self.assertEqual(scores.num_fp_tracks, 1)  # with comatch
-        self.assertEqual(scores.num_fn_tracks, 0)
+        self.assertEqual(scores.num_gt_matched_tracks, 1)
+        self.assertEqual(scores.num_rec_matched_tracks, 2)
+        self.assertEqual(scores.num_rec_tracks, 2)
+        self.assertEqual(scores.num_edge_fps_in_matched_tracks, 0)
+        self.assertEqual(scores.avg_segment_length, 1)
+        self.assertEqual(scores.num_identity_switches, 1)
 
-        self.assertEqual(scores.num_gt_nodes, 6)
-        self.assertEqual(scores.num_matched_nodes, 6)
-        self.assertEqual(scores.num_fp_nodes, 1)
-        self.assertEqual(scores.num_fn_nodes, 0)
+    def test_fn_division_evaluation(self):
+        cells, edges, roi = self.getDivisionTrack()
+        gt_cells = cells.copy()
+        gt_edges = edges.copy()
+        gt_track_graph = self.create_graph(gt_cells, gt_edges, roi)
+        for cell in cells:
+            cell[1]['y'] += 1
+        # introduce a split error
+        edges.remove((5, 2))
+        rec_track_graph = self.create_graph(cells, edges, roi)
+        scores = e.evaluate(
+                gt_track_graph, rec_track_graph, matching_threshold=2)
 
-        self.assertEqual(scores.num_gt_edges, 5)
+        self.assertEqual(scores.num_matched_edges, 5)
+        self.assertEqual(scores.num_fp_edges, 0)
+        self.assertEqual(scores.num_fn_edges, 1)
+        self.assertEqual(scores.num_gt_tracks, 1)
+        self.assertEqual(scores.num_gt_matched_tracks, 1)
+        self.assertEqual(scores.num_rec_matched_tracks, 2)
+        self.assertEqual(scores.num_rec_tracks, 2)
+        self.assertEqual(scores.num_edge_fps_in_matched_tracks, 0)
+        self.assertEqual(scores.avg_segment_length, 2.5)
+        self.assertEqual(scores.num_identity_switches, 1)
+
+    def test_fn_division_evaluation2(self):
+        cells, edges, roi = self.getDivisionTrack()
+        # introduce a false positive edge
+        gt_cells = cells.copy()
+        gt_edges = edges.copy()
+        del gt_cells[0]
+        del gt_edges[0]
+        gt_track_graph = self.create_graph(gt_cells, gt_edges, roi)
+        for cell in cells:
+            cell[1]['y'] += 1
+        # introduce a split error
+        edges.remove((5, 2))
+        rec_track_graph = self.create_graph(cells, edges, roi)
+        scores = e.evaluate(
+                gt_track_graph, rec_track_graph, matching_threshold=2)
+
         self.assertEqual(scores.num_matched_edges, 4)
         self.assertEqual(scores.num_fp_edges, 1)
         self.assertEqual(scores.num_fn_edges, 1)
-
-        self.assertEqual(scores.num_gt_divisions, 1)
-        self.assertEqual(scores.num_matched_divisions, 1)
-        self.assertEqual(scores.num_fp_divisions, 0)
-        self.assertEqual(scores.num_fn_divisions, 0)
-
-    def test_false_division_matching(self):
-        cells, edges, roi = self.getDivisionTrack()
-        gt_cells = cells.copy()
-        gt_edges = edges.copy()
-        gt_edges.remove((5, 2))
-        gt_tracks = self.get_tracks(gt_cells, gt_edges, roi)
-        for cell in cells:
-            cell[1]['y'] += 1
-        rec_tracks = self.get_tracks(cells, edges, roi)
-        match_output = e.match_tracks(gt_tracks, rec_tracks, 2)
-        track_matches, cell_matches, splits, merges, fp, fn = match_output
-
-        expected_track_matches = [(0, 0), (1, 0)]
-        expected_cell_matches = [
-                (1, 1),
-                (2, 2),
-                (3, 3),
-                (4, 4),
-                (5, 5),
-                (6, 6),
-                (7, 7)
-                ]
-
-        self.assertCountEqual(track_matches, expected_track_matches)
-        self.assertCountEqual(cell_matches, expected_cell_matches)
-        self.assertEqual(splits, 0)
-        self.assertEqual(merges, 1)
-        self.assertEqual(fp, 0)
-        self.assertEqual(fn, 0)
-
-    def test_false_division_evaluation(self):
-        cells, edges, roi = self.getDivisionTrack()
-        gt_cells = cells.copy()
-        gt_edges = edges.copy()
-        gt_edges.remove((5, 2))
-        gt_tracks = self.get_tracks(gt_cells, gt_edges, roi)
-        for cell in cells:
-            cell[1]['y'] += 1
-        rec_tracks = self.get_tracks(cells, edges, roi)
-
-        scores = e.evaluate(gt_tracks, rec_tracks, 2)
-        self.assertEqual(scores.num_gt_tracks, 2)
-        self.assertEqual(scores.num_matched_tracks, 2)
-        self.assertEqual(scores.num_splits, 0)
-        self.assertEqual(scores.num_merges, 1)
-        self.assertEqual(scores.num_fp_tracks, 0)
-        self.assertEqual(scores.num_fn_tracks, 0)
-
-        self.assertEqual(scores.num_gt_nodes, 7)
-        self.assertEqual(scores.num_matched_nodes, 7)
-        self.assertEqual(scores.num_fp_nodes, 0)
-        self.assertEqual(scores.num_fn_nodes, 0)
-
-        self.assertEqual(scores.num_gt_edges, 5)
-        self.assertEqual(scores.num_matched_edges, 5)
-        self.assertEqual(scores.num_fp_edges, 1)
-        self.assertEqual(scores.num_fn_edges, 0)
-
-        self.assertEqual(scores.num_gt_divisions, 0)
-        self.assertEqual(scores.num_matched_divisions, 0)
-        self.assertEqual(scores.num_fp_divisions, 1)
-        self.assertEqual(scores.num_fn_divisions, 0)
+        self.assertEqual(scores.num_gt_tracks, 1)
+        self.assertEqual(scores.num_gt_matched_tracks, 1)
+        self.assertEqual(scores.num_rec_matched_tracks, 2)
+        self.assertEqual(scores.num_rec_tracks, 2)
+        self.assertEqual(scores.num_edge_fps_in_matched_tracks, 1)
+        self.assertEqual(scores.avg_segment_length, 2)
+        self.assertEqual(scores.num_identity_switches, 1)
