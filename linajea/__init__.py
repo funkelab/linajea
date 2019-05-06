7ed97477438e4b6c374a265e21ae75ab48427f03
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2019-05-06 13:57:21 -0400

    Remove code only necessary for default model

--- linajea/__init__.py
+++ linajea/__init__.py
@@ -1,6 +1,3 @@
 from __future__ import absolute_import
 from .database import CandidateDatabase
-from .detection import find_cells, find_edges, CellDetectionParameters, EdgeDetectionParameters
-from .target_counts import target_counts
 from .print_time import print_time
-from .downsample_parent_vectors import downsample
