15ef04e49598884e3730fcc3007991441b979ce8
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2019-09-25 14:34:31 -0400

    Update version name to 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
         name='linajea',
-        version='1.1-dev',
+        version='1.1',
         description='Lineage Tracking in 4D Microscopy Volumes.',
         url='https://github.com/funkelab/linajea',
         author='Jan Funke',
