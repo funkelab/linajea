c721e68e371f1e5cf86ed89ae4de95b7e974597d
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2019-11-21 14:58:29 -0500

    Allow passing multiple exclude intervals to random location

--- linajea/gunpowder/random_location_exclude_time.py
+++ linajea/gunpowder/random_location_exclude_time.py
@@ -2,7 +2,8 @@ import gunpowder as gp
 
 
 class RandomLocationExcludeTime(gp.RandomLocation):
-    ''' time interval is like an array slice - includes start, excludes end '''
+    ''' Provide list of time intervals to exclude.
+    time interval is like an array slice - includes start, excludes end '''
 
     def __init__(
             self,
@@ -20,10 +21,20 @@ class RandomLocationExcludeTime(gp.RandomLocation):
             p_nonempty)
 
         self.raw = raw
+        if type(time_interval) is list and \
+            (type(time_interval[0]) is list or
+                type(time_interval[0] is tuple)):
+            self.time_intervals = time_interval
+        else:
+            self.time_intervals = [time_interval]
         self.t_start = time_interval[0]
         self.t_end = time_interval[1]
 
     def accepts(self, request):
-
-        return (request[self.raw].roi.get_begin()[0] >= self.t_end or
-                request[self.raw].roi.get_end()[0] < self.t_start)
+        for interval in self.time_intervals:
+            t_start = interval[0]
+            t_end = interval[1]
+            if not (request[self.raw].roi.get_begin()[0] >= t_end or
+                    request[self.raw].roi.get_end()[0] < t_start):
+                return False
+        return True
