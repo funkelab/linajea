1e3618187ce15803ea739c3a6558d2de3e015b10
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2020-01-07 12:00:12 -0500

    Try to load config files without file extension

--- linajea/load_config.py
+++ linajea/load_config.py
@@ -1,12 +1,24 @@
 import json
 import toml
+import os.path
 
 
 def load_config(f):
-    if f.endswith('.json'):
+    ext = os.path.splitext(f)[1]
+    if ext == '.json':
         config = json.load(f)
-    elif f.endswith('.toml'):
+    elif ext == '.toml':
         config = toml.load(f)
+    elif ext == '':
+        try:
+            config = toml.load(f)
+        except ValueError:
+            try:
+                config = json.load(f)
+            except ValueError:
+                raise ValueError("No file extension provided "
+                                 "and cannot be loaded with json or toml")
     else:
-        raise ValueError("Only json and toml config files supported")
+        raise ValueError("Only json and toml config files supported, not %s"
+                         % ext)
     return config
