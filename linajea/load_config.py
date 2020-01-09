0d4e1b5e6f3b05b9535bf6afff110ed25a0f03e5
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2020-01-09 09:01:37 -0500

    Fix load config function to actually open file

--- linajea/load_config.py
+++ linajea/load_config.py
@@ -3,22 +3,23 @@ import toml
 import os.path
 
 
-def load_config(f):
-    ext = os.path.splitext(f)[1]
-    if ext == '.json':
-        config = json.load(f)
-    elif ext == '.toml':
-        config = toml.load(f)
-    elif ext == '':
-        try:
+def load_config(config_file):
+    ext = os.path.splitext(config_file)[1]
+    with open(config_file, 'r') as f:
+        if ext == '.json':
+            config = json.load(f)
+        elif ext == '.toml':
             config = toml.load(f)
-        except ValueError:
+        elif ext == '':
             try:
-                config = json.load(f)
+                config = toml.load(f)
             except ValueError:
-                raise ValueError("No file extension provided "
-                                 "and cannot be loaded with json or toml")
-    else:
-        raise ValueError("Only json and toml config files supported, not %s"
-                         % ext)
+                try:
+                    config = json.load(f)
+                except ValueError:
+                    raise ValueError("No file extension provided "
+                                     "and cannot be loaded with json or toml")
+        else:
+            raise ValueError("Only json and toml config files supported,"
+                             " not %s" % ext)
     return config
