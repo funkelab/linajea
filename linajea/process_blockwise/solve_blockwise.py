8bda813970b180a416ae8946f7a69e29a4b252a3
Peter Hirsch <peter.hirsch@mdc-berlin.de>
2020-08-14 12:45:40 -0400

    refactor solve: use toml config

--- linajea/process_blockwise/solve_blockwise.py
+++ linajea/process_blockwise/solve_blockwise.py
@@ -1,69 +1,63 @@
-import daisy
+from __future__ import absolute_import
+from copy import deepcopy
 import json
-from linajea import CandidateDatabase
-from .daisy_check_functions import (
-        check_function, write_done,
-        check_function_all_blocks, write_done_all_blocks)
-from linajea.tracking import TrackingParameters, track
 import logging
 import os
 import time
 
+import daisy
+from .daisy_check_functions import (
+        check_function, write_done,
+        check_function_all_blocks, write_done_all_blocks)
+from linajea import CandidateDatabase
+from linajea.tracking import TrackingParameters, track
+from linajea import (adjust_postprocess_roi,
+                     checkOrCreateDB,
+                     load_config)
+
 logger = logging.getLogger(__name__)
 
 
-def solve_blockwise(
-        db_host,
-        db_name,
-        sample,
-        num_workers=8,
-        frames=None,
-        limit_to_roi=None,
-        from_scratch=False,
-        data_dir='../01_data',
-        **kwargs):
+def solve_blockwise(**kwargs):
+    if kwargs.get('validation'):
+        samples = kwargs['data']['val_data_dirs']
+    else:
+        samples = kwargs['data']['test_data_dirs']
 
-    parameters = TrackingParameters(**kwargs)
+    for sample in samples:
+        sample_kwargs = deepcopy(kwargs)
+        solve_blockwise_sample(sample=sample, **sample_kwargs)
+
+def solve_blockwise_sample(**kwargs):
+    if 'db_name' not in kwargs['general']:
+        kwargs['general']['db_name'] = checkOrCreateDB(**kwargs)
+
+    parameters = TrackingParameters(**kwargs['solve'])
     block_size = daisy.Coordinate(parameters.block_size)
     context = daisy.Coordinate(parameters.context)
 
-    # get absolute paths
-    if os.path.isfile(sample) or sample.endswith((".zarr", ".n5")):
-        sample_dir = os.path.abspath(os.path.join(data_dir,
-                                                  os.path.dirname(sample)))
-    else:
-        sample_dir = os.path.abspath(os.path.join(data_dir, sample))
     # get ROI of source
-    with open(os.path.join(sample_dir, 'attributes.json'), 'r') as f:
-        attributes = json.load(f)
-
-    voxel_size = daisy.Coordinate(attributes['resolution'])
-    shape = daisy.Coordinate(attributes['shape'])
-    offset = daisy.Coordinate(attributes['offset'])
+    data_config = load_config(os.path.join(kwargs['sample'],
+                                           "data_config.toml"))
+    voxel_size = daisy.Coordinate(kwargs['data']['voxel_size'])
+    shape = daisy.Coordinate(data_config['general']['shape'])
+    offset = daisy.Coordinate(data_config['general']['offset'])
     source_roi = daisy.Roi(offset, shape*voxel_size)
 
     # determine parameters id from database
     graph_provider = CandidateDatabase(
-        db_name,
-        db_host)
+        kwargs['general']['db_name'],
+        kwargs['general']['db_host'])
     parameters_id = graph_provider.get_parameters_id(parameters)
 
-    if from_scratch:
+    logger.info("from scratch %s", kwargs['solve']['from_scratch'])
+    if kwargs['solve']['from_scratch']:
         graph_provider.set_parameters_id(parameters_id)
         graph_provider.reset_selection()
 
-    # limit to specific frames, if given
-    if frames:
-        logger.info("Solving in frames %s" % frames)
-        begin, end = frames
-        crop_roi = daisy.Roi(
-            (begin, None, None, None),
-            (end - begin, None, None, None))
-        source_roi = source_roi.intersect(crop_roi)
-    # limit to roi, if given
-    if limit_to_roi:
-        logger.info("limiting to roi %s" % str(limit_to_roi))
-        source_roi = source_roi.intersect(limit_to_roi)
+    # limit to specific frames/roi, if given
+    source_roi = adjust_postprocess_roi(source_roi, **kwargs)
+    logger.info("Limiting solving to roi %s", source_roi)
 
     block_write_roi = daisy.Roi(
         (0, 0, 0, 0),
@@ -71,57 +65,54 @@ def solve_blockwise(
     block_read_roi = block_write_roi.grow(
         context,
         context)
-    total_roi = source_roi.grow(
-        context,
-        context)
 
-    logger.info("Solving in %s", total_roi)
     step_name = 'solve_' + str(parameters_id)
-    if check_function_all_blocks(step_name, db_name, db_host):
-        logger.info("Step %s is already completed. Exiting" % step_name)
+    if check_function_all_blocks(step_name,
+                                 kwargs['general']['db_name'],
+                                 kwargs['general']['db_host']):
+        logger.info("Step %s is already completed. Exiting", step_name)
         return True
 
     success = daisy.run_blockwise(
-        total_roi,
+        source_roi,
         block_read_roi,
         block_write_roi,
         process_function=lambda b: solve_in_block(
-            db_host,
-            db_name,
-            parameters,
             b,
+            parameters,
             parameters_id,
-            solution_roi=source_roi),
+            solution_roi=source_roi,
+            **kwargs,
+            ),
         check_function=lambda b: check_function(
             b,
             step_name,
-            db_name,
-            db_host),
-        num_workers=num_workers,
+            kwargs['general']['db_name'],
+            kwargs['general']['db_host']),
+        num_workers=kwargs['solve']['num_workers'],
         fit='overhang')
     if success:
         write_done_all_blocks(
             step_name,
-            db_name,
-            db_host)
+            kwargs['general']['db_name'],
+            kwargs['general']['db_host'])
     logger.info("Finished solving, parameters id is %s", parameters_id)
     return success
 
 
-def solve_in_block(
-        db_host,
-        db_name,
-        parameters,
-        block,
-        parameters_id,
-        solution_roi=None):
+def solve_in_block(block,
+                   parameters,
+                   parameters_id,
+                   solution_roi=None,
+                   **kwargs
+                   ):
     # Solution_roi is the total roi that you want a solution in
     # Limiting the block to the solution_roi allows you to solve
     # all the way to the edge, without worrying about reading
     # data from outside the solution roi
     # or paying the appear or disappear costs unnecessarily
 
-    logger.debug("Solving in block %s", block)
+    logger.info("Solving in block %s", block)
     if solution_roi:
         # Limit block to source_roi
         read_roi = block.read_roi.intersect(solution_roi)
@@ -131,17 +122,24 @@ def solve_in_block(
         write_roi = block.write_roi
 
     graph_provider = CandidateDatabase(
-        db_name,
-        db_host,
+        kwargs['general']['db_name'],
+        kwargs['general']['db_host'],
         mode='r+',
         parameters_id=parameters_id)
     start_time = time.time()
+    nodes_filter = None
+    try:
+        if parameters.masked_nodes is not None:
+            nodes_filter = {parameters.masked_nodes: True}
+    except:
+        pass
     graph = graph_provider.get_graph(
-            read_roi,
-            edge_attrs=["prediction_distance",
-                        "distance",
-                        graph_provider.selected_key]
-            )
+        read_roi,
+        nodes_filter=nodes_filter,
+        edge_attrs=["prediction_distance",
+                    "distance",
+                    graph_provider.selected_key]
+    )
 
     # remove dangling nodes and edges
     dangling_nodes = [
@@ -153,13 +151,15 @@ def solve_in_block(
 
     num_nodes = graph.number_of_nodes()
     num_edges = graph.number_of_edges()
-    logger.info("Reading graph with %d nodes and %d edges took %s seconds"
-                % (num_nodes, num_edges, time.time() - start_time))
+    logger.info("Reading graph with %d nodes and %d edges took %s seconds",
+                num_nodes, num_edges, time.time() - start_time)
 
     if num_edges == 0:
-        logger.info("No edges in roi %s. Skipping"
-                    % read_roi)
-        write_done(block, 'solve_' + str(parameters_id), db_name, db_host)
+        logger.info("No edges in roi %s. Skipping",
+                    read_roi)
+        write_done(block, 'solve_' + str(parameters_id),
+                   kwargs['general']['db_name'],
+                   kwargs['general']['db_host'])
         return 0
 
     frames = [read_roi.get_offset()[0],
@@ -169,9 +169,11 @@ def solve_in_block(
     graph.update_edge_attrs(
             write_roi,
             attributes=[graph_provider.selected_key])
-    logger.info("Updating attribute %s for %d edges took %s seconds"
-                % (graph_provider.selected_key,
-                   num_edges,
-                   time.time() - start_time))
-    write_done(block, 'solve_' + str(parameters_id), db_name, db_host)
+    logger.info("Updating attribute %s for %d edges took %s seconds",
+                graph_provider.selected_key,
+                num_edges,
+                time.time() - start_time)
+    write_done(block, 'solve_' + str(parameters_id),
+               kwargs['general']['db_name'],
+               kwargs['general']['db_host'])
     return 0
