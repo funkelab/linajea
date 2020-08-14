ece21696818682cb1e65ec6333f6493062704fcc
Peter Hirsch <peter.hirsch@mdc-berlin.de>
2020-08-14 11:13:31 -0400

    refactor extract_edges: use toml config

--- linajea/process_blockwise/extract_edges_blockwise.py
+++ linajea/process_blockwise/extract_edges_blockwise.py
@@ -1,102 +1,103 @@
 from __future__ import absolute_import
-from scipy.spatial import KDTree
-import daisy
+from copy import deepcopy
 import json
-import linajea
-from .daisy_check_functions import write_done, check_function
 import logging
-import numpy as np
 import os
 import time
+
+import numpy as np
+from scipy.spatial import KDTree
+
+import daisy
+from .daisy_check_functions import write_done, check_function
+import linajea
+from linajea import (load_config,
+                     checkOrCreateDB,
+                     construct_zarr_filename)
 
 logger = logging.getLogger(__name__)
 
 
-def extract_edges_blockwise(
-        db_host,
-        db_name,
-        sample,
-        edge_move_threshold,
-        block_size,
-        num_workers,
-        frames=None,
-        frame_context=1,
-        data_dir='../01_data',
-        **kwargs):
-
-    # get absolute paths
-    if os.path.isfile(sample) or sample.endswith((".zarr", ".n5")):
-        sample_dir = os.path.abspath(os.path.join(data_dir,
-                                                  os.path.dirname(sample)))
+def extract_edges_blockwise(**kwargs):
+    if kwargs.get('validation'):
+        samples = kwargs['data']['val_data_dirs']
     else:
-        sample_dir = os.path.abspath(os.path.join(data_dir, sample))
+        samples = kwargs['data']['test_data_dirs']
+
+    for sample in samples:
+        sample_kwargs = deepcopy(kwargs)
+        extract_edges_blockwise_sample(sample=sample, **sample_kwargs)
+
+def extract_edges_blockwise_sample(**kwargs):
+    if 'db_name' not in kwargs['general']:
+        kwargs['general']['db_name'] = checkOrCreateDB(**kwargs)
 
     # get ROI of source
-    with open(os.path.join(sample_dir, 'attributes.json'), 'r') as f:
-        attributes = json.load(f)
-
-    voxel_size = daisy.Coordinate(attributes['resolution'])
-    shape = daisy.Coordinate(attributes['shape'])
-    offset = daisy.Coordinate(attributes['offset'])
+    data_config = load_config(os.path.join(kwargs['sample'],
+                                           "data_config.toml"))
+    voxel_size = daisy.Coordinate(kwargs['data']['voxel_size'])
+    shape = daisy.Coordinate(data_config['general']['shape'])
+    offset = daisy.Coordinate(data_config['general']['offset'])
     source_roi = daisy.Roi(offset, shape*voxel_size)
 
     # limit to specific frames, if given
-    if frames:
+    if 'frames' in kwargs['data']:
+        frames = kwargs['data']['frames']
+        logger.info("Limiting extract edges to frames %s" % str(frames))
         begin, end = frames
-        begin -= frame_context
-        end += frame_context
-        crop_roi = daisy.Roi(
+        begin -= kwargs['extract_edges']['frame_context']
+        end += kwargs['extract_edges']['frame_context']
+        frames_roi = daisy.Roi(
             (begin, None, None, None),
             (end - begin, None, None, None))
-        source_roi = source_roi.intersect(crop_roi)
+        source_roi = source_roi.intersect(frames_roi)
 
     # shapes in voxels
     block_write_roi = daisy.Roi(
         (0,)*4,
-        daisy.Coordinate(block_size))
+        daisy.Coordinate(kwargs['extract_edges']['block_size']))
 
-    pos_context = daisy.Coordinate((0,) + (edge_move_threshold,)*3)
-    neg_context = daisy.Coordinate((1,) + (edge_move_threshold,)*3)
+    edge_move_threshold = kwargs['extract_edges']['edge_move_threshold']
+    if isinstance(edge_move_threshold, dict):
+        max_th = max(edge_move_threshold.values())
+    else:
+        max_th = edge_move_threshold
+    pos_context = daisy.Coordinate((0,) + (max_th,)*3)
+    neg_context = daisy.Coordinate((1,) + (max_th,)*3)
     logger.debug("Set neg context to %s", neg_context)
 
     input_roi = source_roi.grow(neg_context, pos_context)
     block_read_roi = block_write_roi.grow(neg_context, pos_context)
 
-    print("Following ROIs in world units:")
-    print("Input ROI       = %s" % input_roi)
-    print("Block read  ROI = %s" % block_read_roi)
-    print("Block write ROI = %s" % block_write_roi)
-    print("Output ROI      = %s" % source_roi)
+    logger.info("Following ROIs in world units:")
+    logger.info("Input ROI       = %s" % input_roi)
+    logger.info("Block read  ROI = %s" % block_read_roi)
+    logger.info("Block write ROI = %s" % block_write_roi)
+    logger.info("Output ROI      = %s" % source_roi)
 
-    print("Starting block-wise processing...")
+    logger.info("Starting block-wise processing...")
+    logger.info("database: %s", kwargs['general']['db_name'])
+    logger.info("sample: %s", kwargs['sample'])
 
     # process block-wise
     daisy.run_blockwise(
         input_roi,
         block_read_roi,
         block_write_roi,
-        process_function=lambda b: extract_edges_in_block(
-            db_name,
-            db_host,
-            edge_move_threshold,
-            b),
+        process_function=lambda b: extract_edges_in_block(b, **kwargs),
         check_function=lambda b: check_function(
             b,
             'extract_edges',
-            db_name,
-            db_host),
-        num_workers=num_workers,
+            kwargs['general']['db_name'],
+            kwargs['general']['db_host']),
+        num_workers=kwargs['prediction']['num_workers'],
         processes=True,
         read_write_conflict=False,
-        fit='shrink')
+        fit='overhang')
+        # fit='shrink')
 
 
-def extract_edges_in_block(
-        db_name,
-        db_host,
-        edge_move_threshold,
-        block):
-
+def extract_edges_in_block(block, **kwargs):
     logger.info(
         "Finding edges in %s, reading from %s",
         block.write_roi, block.read_roi)
@@ -104,14 +105,16 @@ def extract_edges_in_block(
     start = time.time()
 
     graph_provider = linajea.CandidateDatabase(
-        db_name,
-        db_host,
+        kwargs['general']['db_name'],
+        kwargs['general']['db_host'],
         mode='r+')
     graph = graph_provider[block.read_roi]
 
     if graph.number_of_nodes() == 0:
         logger.info("No cells in roi %s. Skipping", block.read_roi)
-        write_done(block, 'extract_edges', db_name, db_host)
+        write_done(block, 'extract_edges',
+                   kwargs['general']['db_name'],
+                   kwargs['general']['db_host'])
         return 0
 
     logger.info(
@@ -163,15 +166,23 @@ def extract_edges_in_block(
         kd_data = [cell[1] for cell in all_pre_cells]
         pre_kd_tree = KDTree(kd_data)
 
+        edge_move_threshold = kwargs['extract_edges']['edge_move_threshold']
         for i, nex_cell in enumerate(cells_by_t[nex]):
 
             nex_cell_id = nex_cell[0]
             nex_cell_center = nex_cell[1]
             nex_parent_center = nex_cell_center + nex_cell[2]
 
+            if isinstance(edge_move_threshold, dict):
+                for th, val in edge_move_threshold.items():
+                    if t < int(th):
+                        emt = val
+                        break
+            else:
+                emt = edge_move_threshold
             pre_cells_indices = pre_kd_tree.query_ball_point(
                 nex_cell_center,
-                edge_move_threshold)
+                emt)
             pre_cells = [all_pre_cells[i] for i in pre_cells_indices]
 
             logger.debug(
@@ -210,5 +221,7 @@ def extract_edges_in_block(
     logger.info(
         "Wrote edges in %.3fs",
         time.time() - start)
-    write_done(block, 'extract_edges', db_name, db_host)
+    write_done(block, 'extract_edges',
+               kwargs['general']['db_name'],
+               kwargs['general']['db_host'])
     return 0
