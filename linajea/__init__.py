3af723342bdfffe10d864d7789391e0ee1386e58
Peter Hirsch <peter.hirsch@mdc-berlin.de>
2020-09-24 11:18:08 -0400

    add function to adjust postprocessing roi

--- linajea/__init__.py
+++ linajea/__init__.py
@@ -3,3 +3,4 @@ from __future__ import absolute_import
 from .candidate_database import CandidateDatabase
 from .print_time import print_time
 from .load_config import load_config
+from .adjust_postprocess_roi import adjust_postprocess_roi
---
+++ linajea/adjust_postprocess_roi.py
@@ -0,0 +1,27 @@
+import daisy
+
+from linajea import parse_limit_roi
+
+def adjust_postprocess_roi(roi, config, use_context=False):
+    if 'limit_to_roi_offset' in config['postprocessing'] or \
+       'frames' in config['postprocessing']:
+        if 'frames' in config['postprocessing']:
+            frames = config['postprocessing']['frames']
+            begin, end = frames
+            if use_context and 'frame_context' in config['extract_edges']:
+                begin -= config['extract_edges']['frame_context']
+                end += config['extract_edges']['frame_context']
+            frames_roi = daisy.Roi(
+                    (begin, None, None, None),
+                    (end - begin, None, None, None))
+            roi = roi.intersect(frames_roi)
+        limit_to_roi = parse_limit_roi(**config['postprocessing'])
+        if limit_to_roi is not None:
+            roi = roi.intersect(limit_to_roi)
+
+        if 'limit_to_roi_hard' not in config['postprocessing'] or \
+           not config['postprocessing']['limit_to_roi_hard']:
+            roi = roi.grow(
+                daisy.Coordinate(config['solve']['context']),
+                daisy.Coordinate(config['solve']['context']))
+    return roi
