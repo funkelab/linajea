24d552e19e6d0b3d28dae8590ea18398ce1027dc
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2021-02-11 14:36:59 -0500

    Add Job config to match spec

--- linajea/config/__init__.py
+++ linajea/config/__init__.py
@@ -2,6 +2,7 @@
 from .linajea_config import LinajeaConfig
 from .general import GeneralConfig
 from .data import DataConfig
+from .job import JobConfig
 from .predict import PredictConfig
 from .extract import ExtractConfig
 from .solve import SolveConfig
---
+++ linajea/config/job.py
@@ -0,0 +1,9 @@
+import attr
+
+
+@attr.s
+class JobConfig:
+    num_workers = attr.ib(type=int)
+    queue = attr.ib(type=str)
+    lab = attr.ib(type=str, default=None)
+    singularity_image = attr.ib(type=str, default=None)
