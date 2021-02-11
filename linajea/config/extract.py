00f608453741adcb0570b328d1f69a47ab2e970b
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2021-02-11 15:10:24 -0500

    Update extract config to match spec

--- linajea/config/extract.py
+++ linajea/config/extract.py
@@ -1,9 +1,22 @@
-from dataclasses import dataclass
-from typing import List
+import attr
+from typing import List, Dict
+
+from .job import JobConfig
+from .utils import ensure_cls
 
 
-@dataclass
+def edge_move_converter():
+    def converter(val):
+        if isinstance(val, int):
+            return {0: val}
+        else:
+            return val
+    return converter
+
+
+@attr.s
 class ExtractConfig:
-    edge_move_threshold: int
-    block_size: List[int]
-    num_workers: int
+    edge_move_threshold = attr.ib(type=Dict[int, int],
+                                  converter=edge_move_converter())
+    block_size = attr.ib(type=List[int])
+    job = attr.ib(converter=ensure_cls(JobConfig))
