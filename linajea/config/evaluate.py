04eaf127a701565ee0164ddfe9b5abf7873f49d5
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2021-02-11 15:19:26 -0500

    Update evaluate config to match spec, allow None in class converter

--- linajea/config/evaluate.py
+++ linajea/config/evaluate.py
@@ -1,8 +1,14 @@
-from dataclasses import dataclass
+import attr
+
+from .data import DataConfig
+from .job import JobConfig
+from .utils import ensure_cls
 
 
-@dataclass
+@attr.s
 class EvaluateConfig:
-    gt_db_name: str
-    from_scratch: bool
-    matching_threshold: int
+    gt_db_name = attr.ib(type=str)
+    matching_threshold = attr.ib(type=int)
+    data = attr.ib(converter=ensure_cls(DataConfig), default=None)
+    job = attr.ib(converter=ensure_cls(JobConfig), default=None)
+    from_scratch = attr.ib(type=bool, default=False)
