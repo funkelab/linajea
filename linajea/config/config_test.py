ded2ade024bb0c57cacd32bbe906b8194456b91f
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2021-02-11 15:10:56 -0500

    Add informal test config and script

---
+++ linajea/config/config_test.py
@@ -0,0 +1,21 @@
+from linajea import load_config
+from linajea.config import (
+        GeneralConfig,
+        DataConfig,
+        JobConfig,
+        PredictConfig,
+        ExtractConfig,
+        )
+
+if __name__ == "__main__":
+    config_dict = load_config("sample_config.toml")
+    general_config = GeneralConfig(**config_dict['general'])
+    print(general_config)
+    data_config = DataConfig(**config_dict['data'])
+    print(data_config)
+    job_config = JobConfig(**config_dict['job'])
+    print(job_config)
+    predict_config = PredictConfig(**config_dict['predict'])
+    print(predict_config)
+    extract_config = ExtractConfig(**config_dict['extract'])
+    print(extract_config)
