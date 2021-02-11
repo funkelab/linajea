d49448a141500ff47d305a852f3c7a4e1a3e60a9
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2021-02-11 12:15:51 -0500

    Add data config to match spec

--- linajea/config/__init__.py
+++ linajea/config/__init__.py
@@ -1,6 +1,7 @@
 # flake8: noqa
 from .linajea_config import LinajeaConfig
 from .general import GeneralConfig
+from .data import DataConfig
 from .predict import PredictConfig
 from .extract import ExtractConfig
 from .solve import SolveConfig
