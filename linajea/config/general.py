e861a9be3948e229bee3cf958834e5123df06955
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2021-02-11 12:07:09 -0500

    Update general config to match spec

--- linajea/config/general.py
+++ linajea/config/general.py
@@ -1,23 +1,12 @@
 import attr
-from attr import validators
-from typing import List
 
 
 @attr.s
 class GeneralConfig:
     setup = attr.ib(type=str)
-    iteration = attr.ib(type=int)
-    sample = attr.ib(type=str)
-    db_host = attr.ib(type=str)
-    db_name = attr.ib(type=str)
-    singularity_image = attr.ib(type=str)
-    data_dir = attr.ib(type=str)
-    data_file = attr.ib(type=str)
     setups_dir = attr.ib(type=str)
-    frames = attr.ib(
-            type=List[int],
-            validator=validators.deep_iterable(
-                member_validator=validators.instance_of(int),
-                iterable_validator=validators.instance_of(list)))
-    queue = attr.ib(type=str, default='gpu_rtx')
-    lab = attr.ib(type=str, default='funke')
+    db_host = attr.ib(type=str)
+    sample = attr.ib(type=str)
+    db_name = attr.ib(type=str, default=None)
+    singularity_image = attr.ib(type=str, default=None)
+    sparse = attr.ib(type=bool, default=True)
