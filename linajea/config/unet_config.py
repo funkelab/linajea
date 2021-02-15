1ec2d2eb82720626e8ace4671cac7d023223ab5f
Peter Hirsch <peter.hirsch@mdc-berlin.de>
2021-02-15 09:45:55 -0500

    add model validators

--- linajea/config/unet_config.py
+++ linajea/config/unet_config.py
@@ -3,21 +3,36 @@ from typing import List
 
 from .data import DataConfig
 from .job import JobConfig
-from .utils import ensure_cls
+from .utils import (ensure_cls,
+                    _check_nd_shape,
+                    _int_list_validator,
+                    _list_int_list_validator)
 
 
 @attr.s(kw_only=True)
 class UnetConfig:
     # shape -> voxels, size -> world units
-    train_input_shape = attr.ib(type=List[int])
-    predict_input_shape = attr.ib(type=List[int])
+    train_input_shape = attr.ib(type=List[int],
+                                validator=[_int_list_validator,
+                                           _check_nd_shape(4)])
+    predict_input_shape = attr.ib(type=List[int],
+                                  validator=[_int_list_validator,
+                                             _check_nd_shape(4)])
     fmap_inc_factors = attr.ib(type=int)
-    downsample_factors = attr.ib(type=List[List[int]])
-    kernel_size_down = attr.ib(type=List[List[int]])
-    kernel_size_up = attr.ib(type=List[List[int]])
-    upsampling = attr.ib(type=str, default="uniform_transposed_conv")
+    downsample_factors = attr.ib(type=List[List[int]],
+                                 validator=_list_int_list_validator)
+    kernel_size_down = attr.ib(type=List[List[int]],
+                               validator=_list_int_list_validator)
+    kernel_size_up = attr.ib(type=List[List[int]],
+                             validator=_list_int_list_validator)
+    upsampling = attr.ib(type=str, default="uniform_transposed_conv",
+                         validator=attr.validators.in_([
+                             "transposed_conv", "resize_conv",
+                             "uniform_transposed_conv"]))
     constant_upsample = attr.ib(type=bool)
-    nms_window_shape = attr.ib(type=List[int])
+    nms_window_shape = attr.ib(type=List[int],
+                               validator=[_int_list_validator,
+                                          _check_nd_shape(3)])
     average_vectors = attr.ib(type=bool, default=False)
     unet_style = attr.ib(type=str, default='split')
     num_fmaps = attr.ib(type=int)
--- linajea/config/utils.py
+++ linajea/config/utils.py
@@ -1,3 +1,6 @@
+import attr
+
+
 def ensure_cls(cl):
     """If the attribute is an instance of cls, pass, else try constructing."""
     def converter(val):
@@ -6,3 +9,19 @@ def ensure_cls(cl):
         else:
             return cl(**val)
     return converter
+
+
+def _check_nd_shape(ndims):
+
+    def _check_shape(self, attribute, value):
+        if len(value) != ndims:
+            raise ValueError("{} must be 4d".format(attribute))
+    return _check_shape
+
+_int_list_validator = attr.validators.deep_iterable(
+    member_validator=attr.validators.instance_of(int),
+    iterable_validator=attr.validators.instance_of(list))
+
+_list_int_list_validator = attr.validators.deep_iterable(
+    member_validator=_int_list_validator,
+    iterable_validator=attr.validators.instance_of(list))
