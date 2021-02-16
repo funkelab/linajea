7d59a2b17236bbfd9baadb89f73efd2c6331fcac
Peter Hirsch <peter.hirsch@mdc-berlin.de>
2021-02-16 08:43:06 -0500

    config: add validate/test, roi to own class, update database config

--- linajea/config/evaluate.py
+++ linajea/config/evaluate.py
@@ -1,14 +1,30 @@
 import attr
 
-from .data import DataConfig
+from .data import DataROIConfig
 from .job import JobConfig
 from .utils import ensure_cls
 
 
-@attr.s
+@attr.s(kw_only=True)
 class EvaluateConfig:
     gt_db_name = attr.ib(type=str)
     matching_threshold = attr.ib(type=int)
-    data = attr.ib(converter=ensure_cls(DataConfig), default=None)
+    roi = attr.ib(converter=ensure_cls(DataROIConfig), default=None)
     job = attr.ib(converter=ensure_cls(JobConfig), default=None)
     from_scratch = attr.ib(type=bool, default=False)
+
+
+@attr.s(kw_only=True)
+class EvaluateTrackingConfig(EvaluateConfig):
+    from_scratch = attr.ib(type=bool, default=False)
+
+
+@attr.s(kw_only=True)
+class EvaluateCellCycleConfig(EvaluateConfig):
+    max_samples = attr.ib(type=int)
+    metric = attr.ib(type=str)
+    use_database = attr.ib(type=bool, default=True)
+    one_off = attr.ib(type=bool)
+    prob_threshold = attr.ib(type=float)
+    dry_run = attr.ib(type=bool)
+    find_fn = attr.ib(type=bool)
