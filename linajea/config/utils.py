4dbfabba3ba140a38564c83ccab8e3cab3da7baf
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2021-02-23 10:40:48 -0500

    Add load_config to config/utils

--- linajea/config/utils.py
+++ linajea/config/utils.py
@@ -1,4 +1,29 @@
 import attr
+import os
+import json
+import toml
+
+
+def load_config(config_file):
+    ext = os.path.splitext(config_file)[1]
+    with open(config_file, 'r') as f:
+        if ext == '.json':
+            config = json.load(f)
+        elif ext == '.toml':
+            config = toml.load(f)
+        elif ext == '':
+            try:
+                config = toml.load(f)
+            except ValueError:
+                try:
+                    config = json.load(f)
+                except ValueError:
+                    raise ValueError("No file extension provided "
+                                     "and cannot be loaded with json or toml")
+        else:
+            raise ValueError("Only json and toml config files supported,"
+                             " not %s" % ext)
+    return config
 
 
 def ensure_cls(cl):
