00088aaaaefb3583b5dd02eed91ec85ff863ba6b
Peter Hirsch <peter.hirsch@mdc-berlin.de>
2021-02-25 06:31:43 -0500

    config: update init, post init hook to get setup

--- linajea/config/__init__.py
+++ linajea/config/__init__.py
@@ -5,19 +5,20 @@ from .cell_cycle_config import CellCycleConfig
 from .cnn_config import (VGGConfig,
                          ResNetConfig,
                          EfficientNetConfig)
-from .data import DataConfig
-from .evaluate import EvaluateConfig
+from .data import DataFileConfig
+from .evaluate import (EvaluateCellCycleConfig,
+                       EvaluateTrackingConfig)
 from .extract import ExtractConfig
 from .general import GeneralConfig
 from .job import JobConfig
-from .linajea_config import LinajeaConfig
+# from .linajea_config import LinajeaConfig
 from .predict import (PredictCellCycleConfig,
                       PredictTrackingConfig)
 from .solve import SolveConfig
 from .tracking_config import TrackingConfig
-from .test import (TestConfig,
-                   TestCellCycleConfig)
+# from .test import (TestTrackingConfig,
+#                    TestCellCycleConfig)
 from .train import (TrainTrackingConfig,
                     TrainCellCycleConfig)
-from .validate import (ValidateConfig,
-                       ValidateCellCycleConfig)
+# from .validate import (ValidateConfig,
+#                        ValidateCellCycleConfig)
