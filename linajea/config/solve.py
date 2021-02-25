a0f1566303e4cd44ceec0f250074886a33683612
Peter Hirsch <peter.hirsch@mdc-berlin.de>
2021-02-25 06:30:49 -0500

    config: separate subsections for eval/solve(tracking) parameters

--- linajea/config/solve.py
+++ linajea/config/solve.py
@@ -5,8 +5,8 @@ from .job import JobConfig
 from .utils import ensure_cls
 
 
-@attr.s
-class SolveConfig:
+@attr.s(kw_only=True)
+class SolveParametersConfig:
     cost_appear = attr.ib(type=float)
     cost_disappear = attr.ib(type=float)
     cost_split = attr.ib(type=float)
@@ -16,7 +16,12 @@ class SolveConfig:
     weight_prediction_distance_cost = attr.ib(type=float)
     block_size = attr.ib(type=List[int])
     context = attr.ib(type=List[int])
-    job = attr.ib(converter=ensure_cls(JobConfig))
     # max_cell_move: currently use edge_move_threshold from extract
     max_cell_move = attr.ib(type=int, default=None)
+
+
+@attr.s(kw_only=True)
+class SolveConfig:
+    job = attr.ib(converter=ensure_cls(JobConfig))
     from_scratch = attr.ib(type=bool, default=False)
+    parameters = attr.ib(converter=ensure_cls(SolveParametersConfig))
