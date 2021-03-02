876713e48767871d7c39c5218386c26c2b58f4a8
Peter Hirsch <peter.hirsch@mdc-berlin.de>
2021-03-02 10:36:38 -0500

    config: predict, add flag to write to db from zarr

--- linajea/config/predict.py
+++ linajea/config/predict.py
@@ -11,11 +11,19 @@ class PredictConfig:
 @attr.s(kw_only=True)
 class PredictTrackingConfig(PredictConfig):
     path_to_script = attr.ib(type=str)
+    path_to_script_db_from_zarr = attr.ib(type=str, default=None)
     write_to_zarr = attr.ib(type=bool, default=False)
     write_to_db = attr.ib(type=bool, default=True)
+    write_db_from_zarr = attr.ib(type=bool, default=False)
     processes_per_worker = attr.ib(type=int, default=1)
     output_zarr_prefix = attr.ib(type=str, default=".")
 
+    def __attrs_post_init__(self):
+        assert self.write_to_zarr or self.write_to_db, \
+            "prediction not written, set write_to_zarr or write_to_db to true!"
+        assert not self.write_db_from_zarr or \
+            self.path_to_script_db_from_zarr, \
+            "supply path_to_script_db_from_zarr if write_db_from_zarr is used!"
 
 @attr.s(kw_only=True)
 class PredictCellCycleConfig(PredictConfig):
