1bc369c0ec86a173d626ef8f339f5ef13817cab7
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2021-07-19 14:12:09 -0400

    Load graph incrementally in greedy tracking

--- linajea/tracking/greedy_track.py
+++ linajea/tracking/greedy_track.py
@@ -1,49 +1,126 @@
-from .track_graph import TrackGraph
 import logging
 import networkx as nx
+from linajea import CandidateDatabase
+from daisy import Roi
+from .track_graph import TrackGraph
 
 logger = logging.getLogger(__name__)
 
 
+def load_graph(
+        cand_db,
+        roi,
+        selected_key):
+    '''
+    Args:
+        cand_db (`linajea.CandidateDatabase`)
+            Candidate Database from which to load graph
+
+        roi (`daisy.Roi`)
+            Roi to get data from.
+
+        selected_key (`str`):
+            Edge attribute used to store selection. Should be set to false by
+            default
+
+    Returns: A new nx.DiGraph with the data from cand_db in region roi.
+    '''
+    edge_attributes = ['distance', 'prediction_distance']
+    graph = cand_db.get_graph(roi, edge_attrs=edge_attributes)
+    # set selected key to false
+    nx.set_edge_attributes(graph, False, selected_key)
+    return graph
+
+
 def greedy_track(
-        graph,
+        db_name,
+        db_host,
         selected_key,
         cell_indicator_threshold,
         metric='prediction_distance',
         frame_key='t',
-        allow_new_tracks=True):
-    if graph.number_of_nodes() == 0:
-        logger.info("No nodes in graph - skipping solving step")
-        return
-    nx.set_edge_attributes(graph, False, selected_key)
-    track_graph = TrackGraph(graph_data=graph,
-                             frame_key=frame_key,
-                             roi=graph.roi)
-    start_frame, end_frame = track_graph.get_frames()
+        allow_new_tracks=True,
+        roi=None):
+    cand_db = CandidateDatabase(db_name, db_host, 'r+')
+    total_roi = cand_db.get_nodes_roi()
+    if roi is not None:
+        total_roi = roi.intersect(total_roi)
 
-    selected_prev_nodes = []
-    for frame in range(end_frame, start_frame + 1, -1):
+    start_frame = total_roi.get_offset()[0]
+    end_frame = start_frame + total_roi.get_shape()[0]
+    logger.info("Tracking from frame %d to frame %d", end_frame, start_frame)
+    step = 10
+    selected_prev_nodes = set()
+    first = True
+    for section_end in range(end_frame, start_frame, -1*step):
+        section_begin = section_end - step
+        frames_roi = Roi((section_begin, None, None, None),
+                         (step, None, None, None))
+        section_roi = total_roi.intersect(frames_roi)
+        logger.info("Greedy tracking in section %s", str(section_roi))
+        selected_prev_nodes = track_section(
+                cand_db,
+                section_roi,
+                selected_key,
+                cell_indicator_threshold,
+                selected_prev_nodes,
+                metric=metric,
+                frame_key=frame_key,
+                allow_new_tracks=allow_new_tracks,
+                first=first)
+        first = False
+        logger.debug("Done tracking in section %s", str(section_roi))
+
+
+def track_section(
+        cand_db,
+        roi,
+        selected_key,
+        cell_indicator_threshold,
+        selected_prev_nodes,
+        metric='prediction_distance',
+        frame_key='t',
+        allow_new_tracks=True,
+        first=False):
+    # this function solves this whole section and stores the result, and
+    # returns the node ids in the preceeding frame (before roi!) that were
+    # selected
+
+    graph = load_graph(cand_db, roi, selected_key)
+    track_graph = TrackGraph(graph_data=graph, frame_key=frame_key, roi=roi)
+    start_frame = roi.get_offset()[0]
+    end_frame = start_frame + roi.get_shape()[0] - 1
+
+    for frame in range(end_frame, start_frame - 1, -1):
+        logger.debug("Processing frame %d", frame)
+
         # find "seed" cells in frame
         seed_candidates = track_graph.cells_by_frame(frame)
-        seeds = [node for node in seed_candidates
-                 if graph.nodes[node]['score'] > cell_indicator_threshold]
+        if len(selected_prev_nodes) > 0:
+            assert [p in seed_candidates for p in selected_prev_nodes],\
+                "previously selected nodes are not contained in current frame!"
+        seeds = set([node for node in seed_candidates
+                     if graph.nodes[node]['score'] > cell_indicator_threshold])
+        logger.debug("Found %d potential seeds in frame %d", len(seeds), frame)
 
         # use only new (not previously selected) nodes to seed new tracks
-        seeds = [s for s in seeds if s not in selected_prev_nodes]
+        seeds = seeds - selected_prev_nodes
+        logger.debug("Found %d seeds in frame %d", len(seeds), frame)
 
-        if frame == end_frame:
+        if first:
             # in this special case, all seeds are treated as selected
             selected_prev_nodes = seeds
-            seeds = []
+            seeds = set()
 
         candidate_edges = []
-        selected_next_nodes = []
+        selected_next_nodes = set()
         # pick the shortest edges greedily for the set of previously selected
         # nodes, with tree constraint (allow divisions)
         for selected_prev in selected_prev_nodes:
             candidate_edges.extend(graph.out_edges(selected_prev, data=True))
 
-        logger.debug("Sorting edges in frame %d", frame)
+        logger.debug("Sorting %d candidate edges in frame %d",
+                     len(candidate_edges), frame)
         sorted_edges = sorted(candidate_edges,
                               key=lambda e: e[2][metric])
 
@@ -65,7 +142,9 @@ def greedy_track(
                 continue
 
             graph.edges[(u, v)][selected_key] = True
-            selected_next_nodes.append(v)
+            selected_next_nodes.add(v)
+
+        logger.debug("Selected %d continuing edges", len(selected_next_nodes))
 
         if allow_new_tracks:
             # pick the shortest edges greedily for the set of new possible
@@ -93,6 +172,13 @@ def greedy_track(
                     continue
 
                 graph.edges[(u, v)][selected_key] = True
-                selected_next_nodes.append(v)
+                selected_next_nodes.add(v)
+        logger.debug("Selected %d total nodes in next frame",
+                     len(selected_next_nodes))
 
         selected_prev_nodes = selected_next_nodes
+    logger.info("updating edges in roi %s" % roi)
+    graph.update_edge_attrs(
+                roi,
+                attributes=selected_key)
+    return selected_prev_nodes
