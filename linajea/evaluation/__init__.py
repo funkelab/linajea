e30a7d4dde8503cbe5cfd2b380de46ee644b72bd
Peter Hirsch <peter.hirsch@mdc-berlin.de>
2022-06-08 12:45:23 -0400

    eval: add func to get score for set of parameters

--- linajea/evaluation/__init__.py
+++ linajea/evaluation/__init__.py
@@ -3,11 +3,13 @@ from .evaluate import evaluate
 from .match import match_edges
 from .match_nodes import match_nodes
 from .evaluate_setup import evaluate_setup
+from .evaluator import Evaluator
 from .report import Report
 from .validation_metric import validation_score
 from .analyze_results import (
         get_result, get_results, get_best_result,
-        get_results_sorted, get_best_result_with_config, get_result_id, get_results_sorted_db,
+        get_results_sorted, get_best_result_with_config,
+        get_result_id, get_result_params, get_results_sorted_db,
         get_best_result_per_setup,
         get_tgmm_results,
         get_best_tgmm_result,
